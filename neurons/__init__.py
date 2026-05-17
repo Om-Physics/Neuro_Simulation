577cb51c7dce8336a535cbfca1e794a7d37e2364
Om Jha <omzha24680@gmail.com>
2026-05-17 12:14:15 +0545

    Delete neurons directory

--- neurons/__init__.py
+++
@@ -1,3 +0,0 @@
-from .hodgkin_huxley import HodgkinHuxley
-from .integrate_fire import LeakyIntegrateAndFire, ExponentialIntegrateAndFire, AdaptiveExponentialIF
-from .base_neuron import BaseNeuron, SpikeRecord
